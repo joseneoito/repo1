8c3611831d9fafa025570bb8797d695d2cc3219b
joseneoito <jose@neoito.com>
2023-08-08 18:49:44 +0530

    feat(import from npm package)

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { body, validationResult } = require('express-validator');
 const { validateRequiredFields, validateRequiredArrays } = require('./sharedModules/validator');
 const utils = require('./sharedModules/queries');
 const Utility = new utils();
+const npm_package = require('@joseneoito/test-npm');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,7 +16,10 @@ app.post('/comments', validateRequiredFields(['body', 'news_uid']), async (req, 
     try {
         const errors = validationResult(req);
         let res = await Utility.getUserByUsername('jose2007kj');
-        console.log("User by Username", res)
+        console.log("User by Username", res, npm_package)
+        res = null;
+        res = await npm_package.Utility.getUserByUsername('jose2007kj');
+        console.log("User by Username from npm package", res)
         if (!errors.isEmpty()) {
             throw errors.array();
         }
